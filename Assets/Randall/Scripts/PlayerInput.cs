7fb44b47d67e0531a30b616cce1bc72fec899bde
Randall Hines <randallhines0@gmail.com>
2018-02-20 12:53:27 -0800

    Start on Player controller

---
+++ Assets/Randall/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Randall {
+	public class PlayerInput : MonoBehaviour {
+
+		static public Vector2 GetMovement () {
+			return new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		}
+
+		static public bool LeftClickDown()
+		{
+			//
+			return Input.GetButtonDown("Fire1");
+		}
+
+		static public bool RightClickDown()
+		{
+			//
+			return Input.GetButtonDown("Fire2");
+		}
+	}
+}
