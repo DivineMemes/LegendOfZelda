30d920cdc9207d4cf25646303da28fb29c296031
Randall Hines <randallhines0@gmail.com>
2018-02-20 16:58:56 -0800

    Added animations and sword direction

--- Assets/Randall/Scripts/PlayerController.cs
+++ Assets/Randall/Scripts/PlayerController.cs
@@ -15,9 +15,14 @@ public class PlayerController : MonoBehaviour, IDamageable {
 
 	[Header ("Combat")]
 	public GameObject sword;
+	public SpriteRenderer swordSprite;
 	public float meleeTime;
 	public float swordAttack;
 	public GameObject shield;
+
+	[Header ("Animation")]
+	public Animator anim;
+	public SpriteRenderer sprite;
 
 	// Use this for initialization
 	void Start () {
@@ -44,11 +49,31 @@ public class PlayerController : MonoBehaviour, IDamageable {
 		//Debug.Log (input);
 
 		rb2D.velocity = input;
-		if (input != Vector2.zero) { orientation = input.normalized; }
+		if (input != Vector2.zero) {
+
+			orientation = input.normalized;
+
+			//Visual Effects
+			anim.SetFloat ("Horizontal", input.x);
+			anim.SetFloat ("Vertical", input.y);
+			if (input.x < 0) { sprite.flipX = true; } else { sprite.flipX = false; }
+			anim.speed = 1;
+		}
+		else
+		{
+			anim.speed = 0;
+		}
 	}
 
 	void Melee () {
 		sword.transform.localPosition = orientation;
+
+		//TODO: Set sword orientation
+		float angle = Vector3.Angle(Vector2.up, orientation);
+		sword.transform.eulerAngles = new Vector3(0,0,orientation.x > 0 ? -1 * angle : 1 * angle);
+		
+		Debug.Log(angle);
+
 		sword.SetActive (true);
 		Invoke ("EndMelee", meleeTime);
 	}
@@ -57,10 +82,9 @@ public class PlayerController : MonoBehaviour, IDamageable {
 		sword.SetActive (false);
 	}
 
-
 	//TODO: Fix this 
 	void Shield () {
-		throw new System.NotImplementedException();
+		throw new System.NotImplementedException ();
 		if (Randall.PlayerInput.RightClickDown ()) {
 			Debug.Log ("Shield up");
 			shield.transform.localPosition = orientation;
