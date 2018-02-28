180b43315814018fb17ea589d995ffa02a022c7f
Randall Hines <randallhines0@gmail.com>
2018-02-28 10:33:42 -0800

    Added heart drops, Boomarang does not collide with water

--- Assets/LegendOfZelda/WyattsStuff/_Scripts/Enemy.cs
+++ Assets/LegendOfZelda/WyattsStuff/_Scripts/Enemy.cs
@@ -2,36 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour, IDamageable
-{
+public class Enemy : MonoBehaviour, IDamageable {
     public float Health;
     public float Dammage;
 
-    public void Damage(float damage)
-    {
+    public AudioSource audioSource;
+    public AudioClip gotHitSound;
+
+    public float heartDropChance = 0.25f;
+    public GameObject heart;
+
+    public void Damage (float damage) {
         Health -= damage;
-        death();
+        if (Health <= 0) {
+            Death ();
+        }
+        
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if(other.collider.tag == ("Player"))
-        {
+    void OnCollisionEnter2D (Collision2D other) {
+        if (other.collider.tag == ("Player")) {
             //Debug.Log("PLAYER YOU BITCH");
-            IDamageable damage = other.collider.transform.parent.GetComponent<IDamageable>();
-            if(damage!=null)
-            {
-                damage.Damage(Dammage);
+            IDamageable damage = other.collider.transform.parent.GetComponent<IDamageable> ();
+            if (damage != null) {
+
+                damage.Damage (Dammage);
             }
         }
     }
 
+    void Death () {
 
-    void death()
-    {
-        if(Health <= 0)
-        {
-            gameObject.SetActive(false);
+        if (heartDropChance < Random.Range (0f, 1f)) {
+            Instantiate (heart, transform.position, Quaternion.identity);
         }
+        gameObject.SetActive (false);
+
     }
 }
