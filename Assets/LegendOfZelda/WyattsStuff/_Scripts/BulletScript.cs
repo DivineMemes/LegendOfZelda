9a1d9e67e3f55a1b1cfc3e250b8ec00c004c634c
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-03-06 14:35:49 -0800

    Dragon AI

---
+++ Assets/LegendOfZelda/WyattsStuff/_Scripts/BulletScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    float timer;
+    public float kill;
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        timer++;
+        if(timer >= kill)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.tag == "Player" || other.collider.tag =="Walls")
+        {
+           Destroy(gameObject);
+        }
+    }
+}
