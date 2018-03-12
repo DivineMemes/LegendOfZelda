681eba679cac3938f2a0adf1acefde8eb2a9f3dd
Unknown <wolfking068@gmail.com>
2018-03-12 10:27:25 -0700

    stuff

---
+++ Assets/Randall/Scripts/BombWall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombWall : MonoBehaviour, IBombable{
+	public SpriteRenderer spriteRenderer;
+	public Collider2D c2D;
+	public void Bombed()
+	{
+		spriteRenderer.sprite = null;
+		c2D.isTrigger = true;
+	}
+}
